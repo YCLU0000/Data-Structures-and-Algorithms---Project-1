6c8f217f42acf788ab286a90523225d997e15ab8
YCLU0000 <54688276+YCLU0000@users.noreply.github.com>
2024-08-12 22:15:36 +0800

    Complete Task0.py

--- Task0.py
+++ Task0.py
@@ -20,3 +20,18 @@ Print messages:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
+first_text = texts[0]
+first_text
+incoming_number = first_text[0]
+answering_number = first_text[1]
+time = first_text[2]
+print(f"First record of texts, <{incoming_number}> texts <{answering_number}> at time <{time}>")
+
+last_call = calls[len(calls)-1]
+last_call
+incoming_number = last_call[0]
+answering_number = last_call[1]
+time = last_call[2]
+during = last_call[3]
+
+print(f"Last record of calls, <{incoming_number}> calls <{answering_number}> at time <{time}>, lasting <{during}> seconds")
